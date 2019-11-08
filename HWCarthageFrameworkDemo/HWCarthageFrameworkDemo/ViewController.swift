4e0f6365a48ca4449a6f9af7142582b5ebce3385
asmakovets <asmakovets@lanit.ru>
2019-11-08 09:56:55 +0300

    [HWSomeFramewor] - создал тестовый проект и вынес framework в отдельный репозиторий

---
+++ HWCarthageFrameworkDemo/HWCarthageFrameworkDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWCarthageFrameworkDemo
+//
+//  Created by Михаил Асмаковец on 08.11.2019.
+//  Copyright © 2019 Михаил Асмаковец. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
