b4571d3cda6c844164d28c293139dcc5a83de6b5
asmakovets <asmakovets@lanit.ru>
2019-11-08 09:20:15 +0300

    [HWSomeFramework] создан framework для использования через carhage

---
+++ HWSomeFramework/HWSomeFramework/HWSomeFramework.h
@@ -0,0 +1,19 @@
+//
+//  HWSomeFramework.h
+//  HWSomeFramework
+//
+//  Created by Михаил Асмаковец on 08.11.2019.
+//  Copyright © 2019 Михаил Асмаковец. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HWSomeFramework.
+FOUNDATION_EXPORT double HWSomeFrameworkVersionNumber;
+
+//! Project version string for HWSomeFramework.
+FOUNDATION_EXPORT const unsigned char HWSomeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HWSomeFramework/PublicHeader.h>
+
+
